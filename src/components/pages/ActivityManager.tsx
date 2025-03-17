6aa80623f5c3cb0ffc17ecf2fd69938736476c04
NEW777 <nwierzbow@gmail.com>
2025-03-17 13:57:20 -0400

    Added Actvity Summary to Actvity manager page

--- src/components/pages/ActivityManager.tsx
+++ src/components/pages/ActivityManager.tsx
@@ -2,13 +2,20 @@ import { options } from "../../data/activities";
 import ActivityLog from "../activities/ActivityLog";
 import DropdownSearch from "../activities/DropdownSearch";
 import Header from "../general/Header";
+import ActivitySummary from "./ActivitySummary";
 
 export default function ActivityManager() {
   return (
-    <div className="flex flex-col p-4 items-center">
-      <Header>Activity Manager</Header>
-      <DropdownSearch options={options}></DropdownSearch>
-      <ActivityLog></ActivityLog>
+    <div className="flex justify-between">
+      <div className="flex flex-col p-4 items-center">
+        <Header>Activity Manager</Header>
+        <DropdownSearch options={options}></DropdownSearch>
+        <ActivityLog></ActivityLog>
+      </div>
+      <div className="flex flex-col p-4 items-center">
+        <Header>Activity Summary</Header>
+        <ActivitySummary></ActivitySummary>
+      </div>
     </div>
   );
 }
---
+++ src/components/pages/ActivitySummary.tsx
@@ -0,0 +1,11 @@
+export default function ActivitySummary() {
+  return (
+    <div className="p-6 bg-gray-100 min-h-screen">
+      <p className="text-gray-700 mb-6">
+        Here you can view a summary of your activities and their impact on the
+        environment.
+      </p>
+      {/* Add additional summary details here */}
+    </div>
+  );
+}
