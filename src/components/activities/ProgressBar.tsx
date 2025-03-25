aa4021da29e73f024887470f0f4e95948244ee31
Nick Wierzbowski <nwierzbow@gmail.com>
2025-03-24 22:08:13 -0400

    Using logged activities to drive bars in activity summary

--- src/components/activities/ProgressBar.tsx
+++ src/components/activities/ProgressBar.tsx
@@ -2,9 +2,10 @@ interface Props {
   value: number; // Current value of the progress
   max: number; // Maximum value of the progress
   color: string; // Color of the progress bar
+  children?: React.ReactNode; // Children of the component
 }
 
-export default function ProgressBar({ value, max, color }: Props) {
+export default function ProgressBar({ value, max, color, children }: Props) {
   // Ensure value doesn't exceed the maximum or fall below 0
   const progress = Math.min(Math.max(value, 0), max);
 
@@ -13,10 +14,11 @@ export default function ProgressBar({ value, max, color }: Props) {
 
   return (
     <div className="w-full bg-gray-300 rounded-md overflow-hidden">
+      
       <div
         className={" h-6 transition-all duration-300 " + color}
         style={{ width: `${widthPercentage}%` }}
-      ></div>
+      >{children}</div>
     </div>
   );
 }
--- src/components/pages/ActivitySummary.tsx
+++ src/components/pages/ActivitySummary.tsx
@@ -1,20 +1,22 @@
-import { categoryColors } from "../../data/categories";
+import { useActivityContext } from "../../context/ActivityContext";
+import { getCategoryColorClass } from "../../data/categories";
 import ProgressBar from "../activities/ProgressBar";
 
 export default function ActivitySummary() {
+  const { activities } = useActivityContext();
   return (
     <div className="p-6 bg-gray-100 min-h-screen">
       <p className="text-gray-700 mb-6">
         Here you can view a summary of your activities and their impact on the
         environment.
       </p>
-      {categoryColors.map((color) => (
+      {activities.map(a => (
         <ProgressBar
-          key={color.colorClass}
-          value={4}
+          key={getCategoryColorClass(a.category)}
+          value={Number.parseFloat(a.carbon_value)}
           max={10}
-          color={color.colorClass}
-        ></ProgressBar>
+          color={getCategoryColorClass(a.category)}
+        >{a.category}</ProgressBar>
       ))}
     </div>
   );
